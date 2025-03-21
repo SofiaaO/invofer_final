ad9179e70543b1fba1c19b59326a2732de9e38af
SofiaaO <sofiaortizh1@gmail.com>
2025-03-21 17:41:59 -0400

    arreglo user

--- mysite/settings.py
+++ mysite/settings.py
@@ -47,15 +47,16 @@ INSTALLED_APPS = [
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # Middleware de WhiteNoise
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'simple_history.middleware.HistoryRequestMiddleware',
 ]
-
 
 ROOT_URLCONF = 'mysite.urls'
 import os
